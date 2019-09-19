d57cadb9940acd125f3b855e21d99718685b28c9
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2019-09-19 15:13:13 +0530

    initial commit

---
+++ ios/Classes/CountlyPlugin.m
@@ -0,0 +1,20 @@
+#import "CountlyPlugin.h"
+
+@implementation CountlyPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"countly"
+            binaryMessenger:[registrar messenger]];
+  CountlyPlugin* instance = [[CountlyPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
