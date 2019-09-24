6d0d694be05ad1b9f7234a690dfb750d47904ac8
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2019-09-24 11:33:04 +0530

    update the countly.dart file add change

--- lib/countly.dart
+++ lib/countly.dart
@@ -92,13 +92,32 @@ static Future<String> setUserData(Map<String, Object> options) async {
     if(options["name"] == null){
       options["name"] = "";
     }
+    if(options["username"] == null){
+      options["username"] = "";
+    }
+    if(options["email"] == null){
+      options["email"] = "";
+    }
+    if(options["organization"] == null){
+      options["organization"] = "";
+    }
+    if(options["phone"] == null){
+      options["phone"] = "";
+    }
+    if(options["picture"] == null){
+      options["picture"] = "";
+    }
+    if(options["picturePath"] == null){
+      options["picturePath"] = "";
+    }
+    
     args.add(options["name"]);
-    args.add(options["username"] || "");
-    args.add(options["email"] || "");
-    args.add(options["organization"] || "");
-    args.add(options["phone"] || "");
-    args.add(options["picture"] || "");
-    args.add(options["picturePath"] || "");
+    args.add(options["username"]);
+    args.add(options["email"]);
+    args.add(options["organization"]);
+    args.add(options["phone"]);
+    args.add(options["picture"]);
+    args.add(options["picturePath"]);
     args.add(options["gender"]);
     args.add(options["byear"]);
 
@@ -151,7 +170,7 @@ static Future<String> setOptionalParametersForInitialization(Map<String, Object>
     if(options["latitude && !options.latitude"].match('\\.')){
         options["latitude"] =   + ".00";
     }
-    if(optionslongitude && !options.longitude.match('\\.')){
+    if(optionslongitude && !options["longitude"].match('\\.')){
         options["longitude"] = latitude + ".00";
     }
 
@@ -159,7 +178,7 @@ static Future<String> setOptionalParametersForInitialization(Map<String, Object>
     args.add(options["country"] || "");
     args.add(options["latitude"] || "0.0");
     args.add(options["longitude"] || "0.0");
-    args.add(String(options.ipAddress) || "0.0.0.0");
+    args.add(String(options["ipAddress"]) || "0.0.0.0");
     final String result = await _channel.invokeMethod('setOptionalParametersForInitialization', <String, dynamic>{
         'data': json(arg)
     });
@@ -210,7 +229,82 @@ static Future<String> setProperty(String keyName , String keyValue) async {
     print(result);
     return result;
   }
-
+  static Future<String> increment(String keyName) async {
+    List <String> arg = [];
+   
+    final String result = await _channel.invokeMethod('increment', <String, dynamic>{
+        'data': json(arg)
+    });
+    print(result);
+    return result;
+  }
+  static Future<String> incrementBy(String keyName, String keyIncrement) async {
+    List <String> arg = [];
+   
+    final String result = await _channel.invokeMethod('incrementBy', <String, dynamic>{
+        'data': json(arg)
+    });
+    print(result);
+    return result;
+  }
+  static Future<String> multiply(String keyName, String multiplyValue) async {
+    List <String> arg = [];
+   
+    final String result = await _channel.invokeMethod('multiply', <String, dynamic>{
+        'data': json(arg)
+    });
+    print(result);
+    return result;
+  }
+  
+  static Future<String> saveMax(String keyName, String saveMax) async {
+    List <String> arg = [];
+   
+    final String result = await _channel.invokeMethod('saveMax', <String, dynamic>{
+        'data': json(arg)
+    });
+    print(result);
+    return result;
+  }
+static Future<String> saveMin(String keyName, String saveMin) async {
+    List <String> arg = [];
+   
+    final String result = await _channel.invokeMethod('saveMin', <String, dynamic>{
+        'data': json(arg)
+    });
+    print(result);
+    return result;
+  }
+static Future<String> setOnce(String keyName, String setOnce) async {
+    List <String> arg = [];
+   
+    final String result = await _channel.invokeMethod('setOnce', <String, dynamic>{
+        'data': json(arg)
+    });
+    print(result);
+    return result;
+  }
+  static Future<String> sendRating(String sendRating) async {
+    List <String> arg = [];
+   
+    final String result = await _channel.invokeMethod('sendRating', <String, dynamic>{
+        'data': json(arg)
+    });
+    print(result);
+    return result;
+  }
+  static Future<String> askForStarRating(callback) async {
+    List <String> arg = [];
+    Countly.rating.create();
+    Countly.rating.set(0);
+    Countly.rating.callback = callback;
+    query('countly-rating-modal').classList.add('open');
+    final String result = await _channel.invokeMethod('askForStarRating', <String, dynamic>{
+        'data': json(arg)
+    });
+    print(result);
+    return result;
+  }
 
   static String json(List <String> list){
     String j = '[';
