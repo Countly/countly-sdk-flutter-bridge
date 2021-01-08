d4f23fd6aa61d6869abd9a50d96c72cd64f1c870
Muhammad Junaid Akram <mjunaid.akram@yahoo.com>
2021-01-08 19:11:39 +0500

    Android underlying sdk updated to 20.11.2

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -31,7 +31,7 @@ class _MyAppState extends State<MyApp> {
         Countly.setLoggingEnabled(true); // Enable countly internal debugging logs
         Countly.enableCrashReporting(); // Enable crash reporting to report unhandled crashes to Countly
         Countly.setRequiresConsent(true); // Set that consent should be required for features to work.
-        Countly.giveConsentInit(["location", "sessions", "attribution", "push", "events", "views", "crashes", "users", "push", "star-rating", "apm", "surveys"]);
+        Countly.giveConsentInit(["location", "sessions", "attribution", "push", "events", "views", "crashes", "users", "push", "star-rating", "apm", "feedback", "remote-config"]);
         Countly.setLocationInit("TR", "Istanbul", "41.0082,28.9784", "10.2.33.12");// Set user initial location.
 
         /// Optional settings for Countly initialisation
