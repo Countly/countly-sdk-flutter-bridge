dd1aba2bdddb24e1675ad746254d3809c3507543
Muhammad Junaid Akram <mjunaid.akram@yahoo.com>
2021-11-10 19:57:54 +0500

    Updated underlying iOS SDK to 21.11.1

--- ios/Classes/CountlyFlutterPlugin.m
+++ ios/Classes/CountlyFlutterPlugin.m
@@ -863,7 +863,7 @@ FlutterMethodChannel* _channel;
             NSException *e = [NSException exceptionWithName:@"Native Exception Crash!" reason:@"Throw Native Exception..." userInfo:nil];
             @throw e;
         });
-    }else if([@"recordAttributionID" isEqualToString:call.method]) {
+    }else if([@"recordIndirectAttribution" isEqualToString:call.method]) {
         dispatch_async(dispatch_get_main_queue(), ^ {
             NSString* attributionID = [command objectAtIndex:0];
             if(CountlyCommon.sharedInstance.hasStarted) {
