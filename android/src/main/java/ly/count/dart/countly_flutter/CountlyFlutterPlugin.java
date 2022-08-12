36a3e1620ce52b4eaf096c2cbde9f7b2f1095954
ijunaid <mjunaid.akram@yahoo.com>
2022-08-12 15:31:57 +0500

    SDK version updated to 22.02.0

--- android/src/main/java/ly/count/dart/countly_flutter/CountlyFlutterPlugin.java
+++ android/src/main/java/ly/count/dart/countly_flutter/CountlyFlutterPlugin.java
@@ -59,7 +59,7 @@ import com.google.firebase.FirebaseApp;
 public class CountlyFlutterPlugin implements MethodCallHandler, FlutterPlugin, ActivityAware, DefaultLifecycleObserver {
 
     private static final String TAG = "CountlyFlutterPlugin";
-    private final String COUNTLY_FLUTTER_SDK_VERSION_STRING = "21.11.2";
+    private final String COUNTLY_FLUTTER_SDK_VERSION_STRING = "22.02.0";
     private final String COUNTLY_FLUTTER_SDK_NAME = "dart-flutterb-android";
     /**
      * Plugin registration.
