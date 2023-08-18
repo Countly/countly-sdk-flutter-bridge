e843e8feb8b65aae42a9069a6a46165b0a67a547
turtledreams <deniztaygunerten@gmail.com>
2023-08-18 21:15:17 +0900

    text correction

--- integration_test/test_utility.dart
+++ integration_test/test_utility.dart
@@ -1,13 +1,23 @@
 import 'package:countly_flutter/countly_config.dart';
 
-// Countly Flutter SDK Test Structure
-// Test Should grouped according to modules
-// Each module would have a separate folder under integration_test
-// In each folder would have a separate file for each test case
-// Each test case file would have a SINGLE!! test case
-// Tests should start by initializing the SDK
-// Then wait for 1 second (or enough)
-// Then tests should follow accordingly
+/*
+Countly Flutter SDK Test Structure
+Test Should grouped according to modules
+Each module would have a separate folder under integration_test
+In each folder would have a separate file for each test case
+Each test case file would have a SINGLE!! test case
+Tests should start by initializing the SDK
+Then wait for 1 second (or enough)
+Then tests should follow accordingly
+*/
+
+/*
+To run the tests at the root of the project run:
+  flutter clean
+  flutter pub get
+  flutter test integration_test
+This would go through each test under the integration_test folder (and sub folders)
+*/
 
 // Constants
 const String SERVER_URL = 'https://xxx.count.ly';
