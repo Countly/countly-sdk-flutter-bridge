2802b72135ea2ca9e81116b200231f7e3f44df95
ijunaid <mjunaid.akram@yahoo.com>
2023-09-18 17:58:33 +0500

    Updated underlying Android SDK version to 23.8.2 (#133)
    * Updated underlying Android SDK version to 23.8.2
    
    * Updated SDK version to 23.8.2

--- android/src/main/java/ly/count/dart/countly_flutter/CountlyFlutterPlugin.java
+++ android/src/main/java/ly/count/dart/countly_flutter/CountlyFlutterPlugin.java
@@ -64,7 +64,7 @@ import com.google.firebase.FirebaseApp;
  */
 public class CountlyFlutterPlugin implements MethodCallHandler, FlutterPlugin, ActivityAware, DefaultLifecycleObserver {
     private static final String TAG = "CountlyFlutterPlugin";
-    private final String COUNTLY_FLUTTER_SDK_VERSION_STRING = "23.8.1";
+    private final String COUNTLY_FLUTTER_SDK_VERSION_STRING = "23.8.2";
     private final String COUNTLY_FLUTTER_SDK_NAME = "dart-flutterb-android";
     private final String COUNTLY_FLUTTER_SDK_NAME_NO_PUSH = "dart-flutterbnp-android";
 
