9c2da0048b4a32e956ea865fc269bb100d26f957
Peter Obiechina <43280227+peterBrxwn@users.noreply.github.com>
2023-10-18 09:49:57 +0100

    moved exports to countly_flutter file (#146)

--- lib/sessions_internal.dart
+++ lib/sessions_internal.dart
@@ -1,6 +1,5 @@
 import 'package:countly_flutter/countly_flutter.dart';
 import 'package:countly_flutter/countly_state.dart';
-import 'package:countly_flutter/sessions.dart';
 
 class SessionsInternal implements Sessions {
   SessionsInternal(this._countlyState);
