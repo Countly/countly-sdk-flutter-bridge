de97c29b34b83ffcc45236b619f727f749ce43a7
turtledreams <deniztaygunerten@gmail.com>
2023-11-10 17:41:15 +0900

    updated script

--- scripts/script.py
+++ scripts/script.py
@@ -50,123 +50,62 @@ FILES_TO_MOVE = [
         '../README.md'
     ]
 ]  # array of, arrays of string tuples. Relative path to the file and the relative path to the copy directory. Something like ['android/sth/sth.txt','android2/folder']
-modPathAndroid = "../android/src/main/java/ly/count/dart/countly_flutter/CountlyFlutterPlugin.java"
-modPathIos = "../ios/Classes/CountlyFlutterPlugin.m"
-modPathCountly = "../lib/countly_flutter.dart"
-modPathCountlyConfig = "../lib/countly_config.dart"
-modPathCountlyState = "../lib/countly_state.dart"
-modPathCountlyRC = "../lib/remote_config.dart"
-modPathCountlyRCInternal = "../lib/remote_config_internal.dart"
-modPathCountlySessionsInternal = "../lib/sessions_internal.dart"
-modPathCountlyUserProfileInternal = "../lib/user_profile_internal.dart"
-modPathCountlyViewsInternal = "../lib/views_internal.dart"
-modPathExampleYaml = "../example/pubspec.yaml"
+# paths to modify
+modPathAndroid = '../android/src/main/java/ly/count/dart/countly_flutter/CountlyFlutterPlugin.java'
+modPathIos = '../ios/Classes/CountlyFlutterPlugin.m'
+modPathCountly = '../lib/countly_flutter.dart'
+modPathExampleYaml = '../example/pubspec.yaml'
+# paths to change packages
+packagePathExample = '../example/lib/'
+packagePathLib = '../lib/'
+packagePathIntegrationTest = '../integration_test/'
+# normal and np package names
+packagePrefix = 'package:countly_flutter/'
+packagePrefixToChange = 'package:countly_flutter_np/'
 objectOfComModification = {
     modPathAndroid: {
-        "modifications": {
-            'import com.google.firebase.iid.FirebaseInstanceId;': "remove",
-            'import com.google.firebase.iid.InstanceIdResult;': "remove",
-            'import com.google.android.gms.tasks.Task;': "remove",
-            'import com.google.android.gms.tasks.OnCompleteListener;': "remove",
-            'import com.google.firebase.FirebaseApp;': "remove",
-            "private final boolean BUILDING_WITH_PUSH_DISABLED = false;": "private final boolean BUILDING_WITH_PUSH_DISABLED = true;"
+        'modifications': {
+            'import com.google.firebase.iid.FirebaseInstanceId;': 'remove',
+            'import com.google.firebase.iid.InstanceIdResult;': 'remove',
+            'import com.google.android.gms.tasks.Task;': 'remove',
+            'import com.google.android.gms.tasks.OnCompleteListener;': 'remove',
+            'import com.google.firebase.FirebaseApp;': 'remove',
+            'private final boolean BUILDING_WITH_PUSH_DISABLED = false;': 'private final boolean BUILDING_WITH_PUSH_DISABLED = true;'
         },
-        "consecutiveOmits": [
-            "FirebaseApp.initializeApp(context);",
-            "FirebaseInstanceId.getInstance().getInstanceId()",
-            ".addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {",
-            "@Override",
-            "public void onComplete(@NonNull Task<InstanceIdResult> task) {",
-            "if (!task.isSuccessful()) {",
+        'consecutiveOmits': [
+            'FirebaseApp.initializeApp(context);',
+            'FirebaseInstanceId.getInstance().getInstanceId()',
+            '.addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {',
+            '@Override',
+            'public void onComplete(@NonNull Task<InstanceIdResult> task) {',
+            'if (!task.isSuccessful()) {',
             'log("getInstanceId failed", task.getException(), LogLevel.WARNING);',
-            "return;",
-            "}",
-            "String token = task.getResult().getToken();",
-            "CountlyPush.onTokenRefresh(token);",
-            "}",
-            "});"
+            'return;',
+            '}',
+            'String token = task.getResult().getToken();',
+            'CountlyPush.onTokenRefresh(token);',
+            '}',
+            '});'
         ]
     },
     modPathIos: {
-        "modifications": {
-            "BOOL BUILDING_WITH_PUSH_DISABLED = false;": "BOOL BUILDING_WITH_PUSH_DISABLED = true;",
-            "// #define COUNTLY_EXCLUDE_PUSHNOTIFICATIONS": "#define COUNTLY_EXCLUDE_PUSHNOTIFICATIONS"
+        'modifications': {
+            'BOOL BUILDING_WITH_PUSH_DISABLED = false;': 'BOOL BUILDING_WITH_PUSH_DISABLED = true;',
+            '// #define COUNTLY_EXCLUDE_PUSHNOTIFICATIONS': '#define COUNTLY_EXCLUDE_PUSHNOTIFICATIONS'
         },
-        "consecutiveOmits": []
+        'consecutiveOmits': []
     },
     modPathCountly: {
-        "modifications": {
-            "import 'package:countly_flutter/countly_config.dart';": "import 'package:countly_flutter_np/countly_config.dart';",
-            "import 'package:countly_flutter/countly_state.dart';": "import 'package:countly_flutter_np/countly_state.dart';",
-            "import 'package:countly_flutter/remote_config.dart';": "import 'package:countly_flutter_np/remote_config.dart';",
-            "import 'package:countly_flutter/remote_config_internal.dart';": "import 'package:countly_flutter_np/remote_config_internal.dart';",
-            "import 'package:countly_flutter/sessions.dart';": "import 'package:countly_flutter_np/sessions.dart';",
-            "import 'package:countly_flutter/sessions_internal.dart';": "import 'package:countly_flutter_np/sessions_internal.dart';",
-            "import 'package:countly_flutter/user_profile.dart';": "import 'package:countly_flutter_np/user_profile.dart';",
-            "import 'package:countly_flutter/user_profile_internal.dart';": "import 'package:countly_flutter_np/user_profile_internal.dart';",
-            "import 'package:countly_flutter/views.dart';": "import 'package:countly_flutter_np/views.dart';",
-            "import 'package:countly_flutter/views_internal.dart';": "import 'package:countly_flutter_np/views_internal.dart';",
-            "export 'package:countly_flutter/countly_config.dart';": "export 'package:countly_flutter_np/countly_config.dart';",
-            "export 'package:countly_flutter/remote_config.dart';": "export 'package:countly_flutter_np/remote_config.dart';",
-            "export 'package:countly_flutter/sessions.dart';": "export 'package:countly_flutter_np/sessions.dart';",
-            "export 'package:countly_flutter/user_profile.dart';": "export 'package:countly_flutter_np/user_profile.dart';",
-            "export 'package:countly_flutter/views.dart';": "export 'package:countly_flutter_np/views.dart';",
-            "static const bool BUILDING_WITH_PUSH_DISABLED = false;": "static const bool BUILDING_WITH_PUSH_DISABLED = true;"
+        'modifications': {
+            'static const bool BUILDING_WITH_PUSH_DISABLED = false;': 'static const bool BUILDING_WITH_PUSH_DISABLED = true;'
         },
-        "consecutiveOmits": []
-    },
-    modPathCountlyConfig: {
-        "modifications": {
-            "import 'package:countly_flutter/countly_flutter.dart';": "import 'package:countly_flutter_np/countly_flutter.dart';",
-        },
-        "consecutiveOmits": []
-    },
-    modPathCountlyState: {
-        "modifications": {
-            "import 'package:countly_flutter/countly_flutter.dart';": "import 'package:countly_flutter_np/countly_flutter.dart';",
-        },
-        "consecutiveOmits": []
-    },
-    modPathCountlyRC: {
-        "modifications": {
-            "import 'package:countly_flutter/experiment_information.dart';": "import 'package:countly_flutter_np/experiment_information.dart';",
-        },
-        "consecutiveOmits": []
-    },
-    modPathCountlyRCInternal: {
-        "modifications": {
-            "import 'package:countly_flutter/countly_flutter.dart';": "import 'package:countly_flutter_np/countly_flutter.dart';",
-            "import 'package:countly_flutter/countly_state.dart';": "import 'package:countly_flutter_np/countly_state.dart';",
-            "import 'package:countly_flutter/experiment_information.dart';": "import 'package:countly_flutter_np/experiment_information.dart';",
-        },
-        "consecutiveOmits": []
-    },
-    modPathCountlySessionsInternal: {
-        "modifications": {
-            "import 'package:countly_flutter/countly_flutter.dart';": "import 'package:countly_flutter_np/countly_flutter.dart';",
-            "import 'package:countly_flutter/countly_state.dart';": "import 'package:countly_flutter_np/countly_state.dart';",
-        },
-        "consecutiveOmits": []
-    },
-    modPathCountlyUserProfileInternal: {
-        "modifications": {
-            "import 'package:countly_flutter/countly_flutter.dart';": "import 'package:countly_flutter_np/countly_flutter.dart';",
-            "import 'package:countly_flutter/countly_state.dart';": "import 'package:countly_flutter_np/countly_state.dart';",
-        },
-        "consecutiveOmits": []
-    },
-    modPathCountlyViewsInternal: {
-        "modifications": {
-            "import 'package:countly_flutter/countly_flutter.dart';": "import 'package:countly_flutter_np/countly_flutter.dart';",
-            "import 'package:countly_flutter/countly_state.dart';": "import 'package:countly_flutter_np/countly_state.dart';",
-        },
-        "consecutiveOmits": []
+        'consecutiveOmits': []
     },
     modPathExampleYaml: {
-        "modifications": {
-            "  countly_flutter:": "  countly_flutter_np:",
+        'modifications': {
+            '  countly_flutter:': '  countly_flutter_np:',
         },
-        "consecutiveOmits": []
+        'consecutiveOmits': []
     }
 }  # 'modifications' is a map with keys are the lines to remove or modify. 'consecutiveOmits' are a block of lines to remove.
 
@@ -191,7 +130,7 @@ def removeFiles(paths, cwd):
     for path in paths:
         path = os.path.join(cwd, path)
         if os.path.exists(path):
-            print("Removing:"+path)
+            print('Removing:'+path)
             os.remove(path)
 
 # loops through the provided array of relative paths and copies each file that exists
@@ -216,21 +155,21 @@ def modifyFile(filePath, modificationInfo, modificationType):
     fileLines = []
     for line in content:
         if modificationType == 'mod':
-            if line.strip("\n") in modificationInfo[filePath]['modifications']:
-                if modificationInfo[filePath]['modifications'][line.strip("\n")] == 'remove':
-                    print("Removing line: ", line)
+            if line.strip('\n') in modificationInfo[filePath]['modifications']:
+                if modificationInfo[filePath]['modifications'][line.strip('\n')] == 'remove':
+                    print('Removing line: ', line)
                 else:
                     fileLines.append(
-                        modificationInfo[filePath]['modifications'][line.strip("\n")] + "\n")
-                    print("Replacing: ["+line+"] with: ["+modificationInfo[filePath]
-                          ['modifications'][line.strip("\n")]+"]")
+                        modificationInfo[filePath]['modifications'][line.strip('\n')] + '\n')
+                    print('Replacing: ['+line+'] with: ['+modificationInfo[filePath]
+                          ['modifications'][line.strip('\n')]+']')
             else:
                 fileLines.append(line)
         elif modificationType == 'bloc':
             if modificationInfo[filePath]['consecutiveOmits']:
-                if modificationInfo[filePath]['consecutiveOmits'][0] in line.strip("\n"):
+                if modificationInfo[filePath]['consecutiveOmits'][0] in line.strip('\n'):
                     modificationInfo[filePath]['consecutiveOmits'].pop(0)
-                    print("Removing line: ", line)
+                    print('Removing line: ', line)
                 else:
                     fileLines.append(line)
             else:
@@ -241,7 +180,7 @@ def modifyFile(filePath, modificationInfo, modificationType):
     finalFile = open(filePath, 'w')
     finalFile.writelines(fileLines)
     finalFile.close()
-    print("Modified the file:"+filePath)
+    print('Modified the file:'+filePath)
     
 def update_package(directory_path, from_package, to_package):
     for root, dirs, files in os.walk(directory_path):
@@ -250,7 +189,7 @@ def update_package(directory_path, from_package, to_package):
 
             # Process only Dart files
             if file_path.endswith('.dart'):
-                print(f"Processing file: {file_path}")
+                print(f'Processing file: {file_path}')
 
                 # Read the file content
                 with open(file_path, 'r') as file:
@@ -266,28 +205,25 @@ def update_package(directory_path, from_package, to_package):
                 with open(file_path, 'w') as file:
                     file.writelines(lines)
 
-                print(f"File processed: {file_path}")
+                print(f'File processed: {file_path}')
 
 def main():
     # give info about set constants
-    print("Paths to erase:")
+    print('Paths to erase:')
     for i in FILES_TO_ERASE:
         print(i, end='\n')
-    print("Paths to copy:")
+    print('Paths to copy:')
     for i in FILES_TO_MOVE:
         print(i, end='\n')
     print('Paths to modify:')
     print(modPathAndroid)
     print(modPathIos)
     print(modPathCountly)
-    print(modPathCountlyState)
-    print(modPathCountlyConfig)
-    print(modPathCountlyRC)
-    print(modPathCountlyRCInternal)
-    print(modPathCountlySessionsInternal)
-    print(modPathCountlyUserProfileInternal)
-    print(modPathCountlyViewsInternal)
     print(modPathExampleYaml)
+    print('Paths to change packages:')
+    print(packagePathExample)
+    print(packagePathLib)
+    print(packagePathIntegrationTest)
 
     # ask for permission to run the script
     start = input('Do you want to continue? (y/n)')
@@ -295,23 +231,20 @@ def main():
         cwd = os.getcwd()  # get current working directory
         removeFiles(FILES_TO_ERASE, cwd)  # remove files
         copyFiles(FILES_TO_MOVE, cwd)  # copies a file
+        # modify files
         modifyFile(modPathAndroid, objectOfComModification, 'mod')
         modifyFile(modPathAndroid, objectOfComModification, 'bloc')
         modifyFile(modPathIos, objectOfComModification, 'mod')
         modifyFile(modPathCountly, objectOfComModification, 'mod')
-        modifyFile(modPathCountlyConfig, objectOfComModification, 'mod')
-        modifyFile(modPathCountlyState, objectOfComModification, 'mod')
-        modifyFile(modPathCountlyRC, objectOfComModification, 'mod')
-        modifyFile(modPathCountlyRCInternal, objectOfComModification, 'mod')
-        modifyFile(modPathCountlySessionsInternal, objectOfComModification, 'mod')
-        modifyFile(modPathCountlyUserProfileInternal, objectOfComModification, 'mod')
-        modifyFile(modPathCountlyViewsInternal, objectOfComModification, 'mod')
         modifyFile(modPathExampleYaml, objectOfComModification, 'mod')
-        update_package("../example/lib/", 'package:countly_flutter/', 'package:countly_flutter_np/')
-        print("Done")
+        # np package update
+        update_package(packagePathExample, packagePrefix, packagePrefixToChange)
+        update_package(packagePathLib, packagePrefix, packagePrefixToChange)
+        update_package(packagePathIntegrationTest, packagePrefix, packagePrefixToChange)
+        print('Done')
     else:
-        print("Aborted")
+        print('Aborted')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
