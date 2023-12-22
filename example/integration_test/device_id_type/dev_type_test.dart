e8d76512a0d8cea2853d7eeef1a667a75dd57a00
turtledreams <deniztaygunerten@gmail.com>
2023-12-22 22:44:14 +0900

    tests

---
+++ example/integration_test/device_id_type/dev_type_test.dart
@@ -0,0 +1,24 @@
+// This is a basic Flutter integration test.
+//
+// Since integration tests run in a full Flutter application, they can interact
+// with the host side of a plugin implementation, unlike Dart unit tests.
+//
+// For more information about Flutter integration tests, please see
+// https://docs.flutter.dev/cookbook/testing/integration/introduction
+
+import 'package:countly_flutter/countly_flutter.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+final String SERVER_URL = 'https://xxx.count.ly'; // MANDATORY !!
+final String APP_KEY = 'YOUR_APP_KEY'; // MANDATORY !!
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  testWidgets('Test Device ID Type - Dev', (WidgetTester tester) async {
+    CountlyConfig config = CountlyConfig(SERVER_URL, APP_KEY)..setLoggingEnabled(true).setDeviceId('test');
+    await Countly.initWithConfig(config);
+    DeviceIdType? type = await Countly.getDeviceIDType();
+    expect(type, DeviceIdType.DEVELOPER_SUPPLIED);
+  });
+}
