6cecf6cbf8f6a8f1f02480c84ca1311619c8d91e
turtledreams <deniztaygunerten@gmail.com>
2023-12-27 21:36:23 +0900

    comments

--- example/integration_test/device_id_type/dev_type_test.dart
+++ example/integration_test/device_id_type/dev_type_test.dart
@@ -1,17 +1,9 @@
-// This is a basic Flutter integration test.
-//
-// Since integration tests run in a full Flutter application, they can interact
-// with the host side of a plugin implementation, unlike Dart unit tests.
-//
-// For more information about Flutter integration tests, please see
-// https://docs.flutter.dev/cookbook/testing/integration/introduction
-
 import 'package:countly_flutter/countly_flutter.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-final String SERVER_URL = 'https://xxx.count.ly'; // MANDATORY !!
-final String APP_KEY = 'YOUR_APP_KEY'; // MANDATORY !!
+final String SERVER_URL = 'https://xxx.count.ly';
+final String APP_KEY = 'YOUR_APP_KEY';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
