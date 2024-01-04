1d702ec4b06f48f6228f56b9ce858983d6faa3fd
turtledreams <62231246+turtledreams@users.noreply.github.com>
2024-01-04 22:16:30 +0900

    Tests (#181)
    * tests
    
    * query tests
    
    * comments
    
    * gap
    
    * some commencts
    
    * utils
    
    * improvements
    
    * numbers

---
+++ example/integration_test/scenario_device_id_init/1.dev_type_test.dart
@@ -0,0 +1,20 @@
+import 'package:countly_flutter/countly_flutter.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import '../utils.dart';
+
+/// 1.Check if setDeviceId() sets the device ID correctly and the type is DEVELOPER_SUPPLIED
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  testWidgets('1.Test Device ID Type - Developer sets the ID during init', (WidgetTester tester) async {
+    // Initialize the SDK
+    CountlyConfig config = CountlyConfig(SERVER_URL, APP_KEY).setLoggingEnabled(true).setDeviceId('test');
+    await Countly.initWithConfig(config);
+    // Get the device ID type
+    DeviceIdType? type = await Countly.getDeviceIDType();
+    String? id = await Countly.getCurrentDeviceId();
+    // Verify the device ID type
+    expect(type, DeviceIdType.DEVELOPER_SUPPLIED);
+    expect(id, 'test');
+  });
+}
---
+++ example/integration_test/utils.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+
+// Base config options for tests
+final String SERVER_URL = 'https://xxx.count.ly';
+final String APP_KEY = 'YOUR_APP_KEY';
+
+/// Verify the common request parameters
+void testCommonRequestParams(Map<String, List<String>> requestObject) {
+  expect(requestObject['app_key']?[0], 'YOUR_APP_KEY');
+  expect(requestObject['sdk_name']?[0], 'dart-flutterb-android');
+  expect(requestObject['sdk_version']?[0], '23.12.0');
+  expect(requestObject['av']?[0], '1.0.0');
+
+  assert(requestObject['device_id']?[0] != null);
+  assert(requestObject['timestamp']?[0] != null);
+  assert(requestObject['checksum256']?[0] != null);
+
+  // healthcheck does not have rr
+  if (requestObject['hc'] == null) {
+    assert(requestObject['rr']?[0] != null);
+  }
+
+  expect(requestObject['hour']?[0], DateTime.now().hour.toString());
+  expect(requestObject['dow']?[0], DateTime.now().weekday.toString());
+  expect(requestObject['tz']?[0], DateTime.now().timeZoneOffset.inMinutes.toString());
+}
+
+/// Start a server to receive the requests from the SDK and store them in a provided List
+/// Use http://0.0.0.0:8080 as the server url
+void creatServer(List requestArray) async {
+  // Start a server to receive the requests from the SDK
+  var server = await HttpServer.bind(InternetAddress.anyIPv4, 8080);
+  server.listen((HttpRequest request) {
+    print(request.uri.queryParametersAll.toString());
+
+    // Store the request parameters for later verification
+    requestArray.add(request.uri.queryParametersAll);
+
+    // Send a response
+    request.response.statusCode = HttpStatus.ok;
+    request.response.headers.contentType = ContentType.json;
+    request.response.headers.set('Access-Control-Allow-Origin', '*');
+    request.response.write(jsonEncode({'result': 'Success'}));
+    request.response.close();
+  });
+}
