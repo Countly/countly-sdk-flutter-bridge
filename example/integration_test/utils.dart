3530517f834374088374d600071aa269fd261949
turtledreams <deniztaygunerten@gmail.com>
2024-01-25 22:14:52 +0900

    app key

--- example/integration_test/utils.dart
+++ example/integration_test/utils.dart
@@ -24,7 +24,7 @@ Future<List<String>> getEventQueue() async {
 
 /// Verify the common request queue parameters
 void testCommonRequestParams(Map<String, List<String>> requestObject) {
-  expect(requestObject['app_key']?[0], 'YOUR_APP_KEY');
+  expect(requestObject['app_key']?[0], APP_KEY);
   expect(requestObject['sdk_name']?[0], "dart-flutterb-${Platform.isIOS ? "ios" : "android"}");
   expect(requestObject['sdk_version']?[0], '23.12.1');
   expect(requestObject['av']?[0], Platform.isIOS ? '0.0.1' : '1.0.0');
