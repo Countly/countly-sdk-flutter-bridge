e993c60d1e57b61811fb1976b01546db43f7eda2
Peter Obiechina <obiechinapeter2@gmail.com>
2024-07-01 15:00:00 +0100

    Work in progres

--- example/lib/config_object.dart
+++ example/lib/config_object.dart
@@ -30,53 +30,54 @@ class CountlyConfiguration {
   };
   static final GlobalCrashFilterCallback globalCrashFilterCallback = (CrashData crashData) {
     // your logic to determine if crashData should be manipulated.
-    if (crashData.exception.contains('sensitive-data')) {
+    if (crashData.stackTrace.contains('sensitive-data')) {
       // return an updated crashData or null if you want the crash data to be ignored.
-      return crashData.copyWith(exception: 'filtered-data');
+      // return false;
+      return crashData.copyWith(stackTrace: '*****');
     }
 
     return crashData;
   };
 
   static CountlyConfig getConfig() {
-
     if (SERVER_URL == 'https://your.server.ly' || APP_KEY == 'YOUR_APP_KEY') {
       print('Please do not use default set of app key and server url');
     }
 
-    return CountlyConfig(SERVER_URL, APP_KEY)..setLoggingEnabled(true) // Enable countly internal debugging logs
+    return CountlyConfig(SERVER_URL, APP_KEY)
+          ..setLoggingEnabled(true) // Enable countly internal debugging logs
 
-        // Currently only logging is enabled for debugging purposes
-        // Below you can see most of the methods that you can use to configure the Countly SDK
+          // Currently only logging is enabled for debugging purposes
+          // Below you can see most of the methods that you can use to configure the Countly SDK
 
-        // Remote Config related methods
-        //------------------------------------------------------------
-        //   ..enableRemoteConfigAutomaticTriggers()
-        //   ..enableRemoteConfigValueCaching()
-        //   ..enrollABOnRCDownload() // This is for specific circumstances only
-        //   ..remoteConfigRegisterGlobalCallback(callback) // Set Automatic value download happens when the SDK is initiated or when the device ID is changed.
-        //------------------------------------------------------------
+          // Remote Config related methods
+          //------------------------------------------------------------
+          //   ..enableRemoteConfigAutomaticTriggers()
+          //   ..enableRemoteConfigValueCaching()
+          //   ..enrollABOnRCDownload() // This is for specific circumstances only
+          //   ..remoteConfigRegisterGlobalCallback(callback) // Set Automatic value download happens when the SDK is initiated or when the device ID is changed.
+          //------------------------------------------------------------
 
-        //   ..enableCrashReporting() // Enable crash reporting to report unhandled crashes to Countly
-        //   ..setRequiresConsent(true) // Set that consent should be required for features to work.
-        //   ..giveAllConsents() // Either use giveAllConsents or setConsentEnabled
-        //   ..setConsentEnabled([
-        //     CountlyConsent.sessions,
-        //     CountlyConsent.events,
-        //     CountlyConsent.views,
-        //   ]) // for giving specific consent
-        // ..setLocation(countryCode: 'KR', city: 'Seoul', gpsCoordinates: '41.0082,28.9784', ipAddress: '10.2.33.12') // Set user  location.
-        //   ..setCustomCrashSegment(crashSegment)
-        //   ..setUserProperties(userProperties)
-        //   ..recordIndirectAttribution(attributionValues)
-        //   ..recordDirectAttribution('countly', campaignData)
-        //   ..setRecordAppStartTime(true) // Enable APM features, which includes the recording of app start time.
-        //   ..setStarRatingTextMessage('Message for start rating dialog')
-        //   ..setParameterTamperingProtectionSalt('salt') // Set the optional salt to be used for calculating the checksum of requested data which will be sent with each request
-        //   ..enableManualSessionHandling() // Enable manual session handling
-        //   ..setHttpPostForced(false) // Set to 'true' if you want HTTP POST to be used for all requests
-        // ..disableLocation() // Call if you want to disable location tracking
-        // ..setGlobalCrashFilterCallback(globalCrashFilterCallback) // Call if you want to filter crash data
+          //   ..enableCrashReporting() // Enable crash reporting to report unhandled crashes to Countly
+          //   ..setRequiresConsent(true) // Set that consent should be required for features to work.
+          //   ..giveAllConsents() // Either use giveAllConsents or setConsentEnabled
+          //   ..setConsentEnabled([
+          //     CountlyConsent.sessions,
+          //     CountlyConsent.events,
+          //     CountlyConsent.views,
+          //   ]) // for giving specific consent
+          // ..setLocation(countryCode: 'KR', city: 'Seoul', gpsCoordinates: '41.0082,28.9784', ipAddress: '10.2.33.12') // Set user  location.
+          //   ..setCustomCrashSegment(crashSegment)
+          //   ..setUserProperties(userProperties)
+          //   ..recordIndirectAttribution(attributionValues)
+          //   ..recordDirectAttribution('countly', campaignData)
+          //   ..setRecordAppStartTime(true) // Enable APM features, which includes the recording of app start time.
+          //   ..setStarRatingTextMessage('Message for start rating dialog')
+          //   ..setParameterTamperingProtectionSalt('salt') // Set the optional salt to be used for calculating the checksum of requested data which will be sent with each request
+          //   ..enableManualSessionHandling() // Enable manual session handling
+          //   ..setHttpPostForced(false) // Set to 'true' if you want HTTP POST to be used for all requests
+          // ..disableLocation() // Call if you want to disable location tracking
+          ..setGlobalCrashFilterCallback(globalCrashFilterCallback) // Call if you want to filter crash data
         ;
   }
 }
