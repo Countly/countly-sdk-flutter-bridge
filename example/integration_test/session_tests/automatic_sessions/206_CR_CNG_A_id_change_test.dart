1c33c0457bf23f187fd20fbd64bec4656a7d5007
turtledreams <62231246+turtledreams@users.noreply.github.com>
2024-07-01 16:07:56 +0900

    206 android

--- example/integration_test/session_tests/automatic_sessions/206_CR_CNG_A_id_change_test.dart
+++ example/integration_test/session_tests/automatic_sessions/206_CR_CNG_A_id_change_test.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:countly_flutter/countly_flutter.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -33,14 +36,8 @@ void main() {
     await tester.pump(Duration(seconds: 1));
     await Countly.changeDeviceId('newID_2', false);
 
-    FlutterForegroundTask.minimizeApp();
     await tester.pump(Duration(seconds: 1));
-    FlutterForegroundTask.launchApp();
     await Countly.changeDeviceId('newID', true);
-    FlutterForegroundTask.minimizeApp();
-    await tester.pump(Duration(seconds: 1));
-    FlutterForegroundTask.launchApp();
-    await tester.pump(Duration(seconds: 1));
 
     // Get request and event queues from native side
     List<String> requestList = await getRequestQueue(); // List of strings
@@ -54,18 +51,32 @@ void main() {
 
     // Currently
     // - consents
-    // - location
-    // - change ID
-    // - consents
+    // - location (android only)
     // - change ID
     // - change ID
-    // seems plausible due to known issues
-    // expect(requestList.length, 1);
+    // - change ID
+    expect(requestList.length, Platform.isAndroid ? 5 : 4);
 
     var i = 0;
     for (var element in requestList) {
       Map<String, List<String>> queryParams = Uri.parse("?" + element).queryParametersAll;
-      // TODO: write these after RQ migration
+      testCommonRequestParams(queryParams); // tests
+      if (i == 0) {
+        // example:
+        // consent: [{"sessions":true,"crashes":true,"users":true,"push":true,"feedback":true,"scrolls":true,"remote-config":true,"attribution":true,"clicks":true,"location":true,"star-rating":true,"events":true,"views":true,"apm":true}]
+        Map<String, dynamic> consentInRequest = jsonDecode(queryParams['consent']![0]);
+        for (var key in ['push', 'feedback', 'crashes', 'attribution', 'users', 'events', 'remote-config', 'sessions', 'location', 'views', 'apm']) {
+          expect(consentInRequest[key], false);
+        }
+        expect(consentInRequest.length, Platform.isAndroid ? 14 : 11);
+      } else if ((Platform.isAndroid && (i == 2 || i == 3 || i == 4)) || (Platform.isIOS && (i == 1 || i == 2 || i == 3))) {
+        expect(queryParams['old_device_id']?[0].isNotEmpty, true);
+        expect(queryParams['device_id']?[0], 'newID');
+      } else if (Platform.isAndroid && (i == 1)) {
+        expect(queryParams['location'], ['']);
+        expect(queryParams['device_id']?[0] != 'newID', true);
+      }
+
       print('RQ.$i: $queryParams');
       print('========================');
       i++;
