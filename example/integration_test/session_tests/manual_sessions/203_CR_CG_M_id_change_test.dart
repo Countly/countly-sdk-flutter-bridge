c362bc5005f6756878fda8263c3085ed5534867a
ijunaid <mjunaid.akram@yahoo.com>
2024-07-02 13:07:11 +0500

    Changes after running script

--- example/integration_test/session_tests/manual_sessions/203_CR_CG_M_id_change_test.dart
+++ example/integration_test/session_tests/manual_sessions/203_CR_CG_M_id_change_test.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
-import 'package:countly_flutter/countly_flutter.dart';
+import 'package:countly_flutter_np/countly_flutter.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import '../../utils.dart';
