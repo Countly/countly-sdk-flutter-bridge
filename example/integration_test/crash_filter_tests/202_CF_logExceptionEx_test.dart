4c301fd55959954be5acfdafe4358e4de42234cc
Peter Obiechina <obiechinapeter2@gmail.com>
2024-07-30 15:07:17 +0100

    added tests for globalCrashfiltercallback

---
+++ example/integration_test/crash_filter_tests/202_CF_logExceptionEx_test.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+import 'package:countly_flutter/countly_flutter.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import '../utils.dart';
+
+/// Test globalCrashFilterCallback when error is recorded from logExceptionEx
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  testWidgets('202_CF_logExceptionEx_test', (WidgetTester tester) async {
+    final errorString = 'Division';
+    final filterString = '*****';
+    final GlobalCrashFilterCallback globalCrashFilterCallback = (crash) {
+      // Check that the error string exists in the stackTrace
+      expect(crash.stackTrace.contains(errorString), true);
+      expect(crash.stackTrace.contains(filterString), false);
+
+      return crash.copyWith(stackTrace: crash.stackTrace.replaceAll(errorString, filterString));
+    };
+
+    // Initialize the SDK
+    CountlyConfig config = CountlyConfig(SERVER_URL, APP_KEY).setLoggingEnabled(true).setGlobalCrashFilterCallback(globalCrashFilterCallback).enableCrashReporting();
+    await Countly.initWithConfig(config);
+
+    try {
+      int firstInput = 20;
+      int secondInput = 0;
+      int result = firstInput ~/ secondInput;
+      print('The result of $firstInput divided by $secondInput is $result');
+    } catch (e, s) {
+      Countly.logExceptionEx(e as Exception, true, stacktrace: s);
+    }
+    await Future.delayed(Duration(seconds: 5));
+
+    // Get request and event queues from native side
+    List<String> requestList = await getRequestQueue(); // List of strings
+
+    // Some logs for debugging
+    print('RQ: $requestList');
+    print('RQ length: ${requestList.length}');
+    expect(requestList.length, 2);
+
+    for (int i = 0; i < requestList.length; i++) {
+      Map<String, List<String>> queryParams = Uri.parse('?' + requestList[i]).queryParametersAll;
+      print(queryParams);
+      print(i);
+      testCommonRequestParams(queryParams);
+      if (i == 0) {
+        expect(queryParams['begin_session']?[0], '1');
+      } else if (i == 1) {
+        final Map<String, dynamic> crash = json.decode(queryParams['crash']![0]);
+        // Check that the error string has been replace in the Global callback
+        expect((crash['_error'] as String).contains(errorString), false);
+        expect((crash['_error'] as String).contains(filterString), true);
+      }
+      print('RQ.$i: $queryParams');
+      print('========================');
+    }
+  });
+}
---
+++ example/integration_test/crash_filter_tests/notes.md
@@ -0,0 +1,14 @@
+Init options:
+M:Manual Sessions enabled
+A:Automatic sessions enabled
+H:Hybrid Sessions enabled
+CR:Consent Required
+CNR:Consent not Required
+CG:Session Consent given
+CNG:Session Consent not given
+
+## Anomalies
+
+Android:
+
+iOS:
