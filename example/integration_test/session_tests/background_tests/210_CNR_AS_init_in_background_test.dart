513c891bbefbbd4bbf9a9d26b693031c09a3c817
ijunaid <mjunaid.akram@yahoo.com>
2024-07-30 14:02:52 +0500

    210 test updated

--- example/integration_test/session_tests/background_tests/210_CNR_AS_init_in_background_test.dart
+++ example/integration_test/session_tests/background_tests/210_CNR_AS_init_in_background_test.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'dart:io';
 import 'package:countly_flutter/countly_flutter.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -9,13 +9,8 @@ import '../../utils.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   testWidgets('210_CNR_AS_init_in_background_test', (WidgetTester tester) async {
-    List<String> requestList = await getRequestQueue(); // List of strings
-    List<String> eventList = await getEventQueue(); // List of json objects
-
-    expect(requestList.length, 0);
-    expect(eventList.length, 0);
     FlutterForegroundTask.minimizeApp();
-    await tester.pump(Duration(seconds: 1));
+    await tester.pump(Duration(seconds: 2));
 
     // Initialize the SDK
     CountlyConfig config = CountlyConfig(SERVER_URL, APP_KEY).setLoggingEnabled(true);
@@ -23,13 +18,20 @@ void main() {
     await Future.delayed(const Duration(seconds: 1));
 
     // Get request and event queues from native side
-    requestList = await getRequestQueue(); // List of strings
-    eventList = await getEventQueue(); // List of json objects
+    List<String> requestList = await getRequestQueue(); // List of strings
+    List<String> eventList = await getEventQueue(); // List of json objects
+
+    print('RQ: $requestList');
+    print('RQ length: ${requestList.length}');
+    print('EQ: $eventList');
+    print('EQ length: ${eventList.length}');
 
     expect(requestList.length, 0);
     expect(eventList.length, 0);
-
-    await tester.pump(Duration(seconds: 1));
+    if (Platform.isIOS) {
+      printMessageMultipleTimes('will now go to background, get ready to go foreground manually', 3);
+    }
+    await tester.pump(Duration(seconds: 3));
     FlutterForegroundTask.launchApp();
     await tester.pump(Duration(seconds: 1));
 
