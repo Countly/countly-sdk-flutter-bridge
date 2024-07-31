4813e2952dd1f36a748a1de9ef9b9d8b843155de
Peter Obiechina <obiechinapeter2@gmail.com>
2024-07-31 15:41:06 +0100

    updated to 24.7.1

--- ios/Classes/CountlyFlutterPlugin.m
+++ ios/Classes/CountlyFlutterPlugin.m
@@ -28,7 +28,7 @@ BOOL BUILDING_WITH_PUSH_DISABLED = false;
 
 CLYPushTestMode const CLYPushTestModeProduction = @"CLYPushTestModeProduction";
 
-NSString *const kCountlyFlutterSDKVersion = @"24.7.0";
+NSString *const kCountlyFlutterSDKVersion = @"24.7.1";
 NSString *const kCountlyFlutterSDKName = @"dart-flutterb-ios";
 NSString *const kCountlyFlutterSDKNameNoPush = @"dart-flutterbnp-ios";
 
