6f49fd65f49a5ed6aea4c0a425ef42ba7749ba8d
turtledreams <62231246+turtledreams@users.noreply.github.com>
2024-09-07 00:42:36 +0900

    consent

--- example/integration_test/session_tests/manual_sessions/203_CR_CG_M_id_change_test.dart
+++ example/integration_test/session_tests/manual_sessions/203_CR_CG_M_id_change_test.dart
@@ -77,10 +77,10 @@ void main() {
         // example:
         // consent: [{"sessions":true,"crashes":true,"users":true,"push":true,"feedback":true,"scrolls":true,"remote-config":true,"attribution":true,"clicks":true,"location":true,"star-rating":true,"events":true,"views":true,"apm":true}]
         Map<String, dynamic> consentInRequest = jsonDecode(queryParams['consent']![0]);
-        for (var key in ['push', 'feedback', 'crashes', 'attribution', 'users', 'events', 'remote-config', 'sessions', 'location', 'views', 'apm']) {
+        for (var key in ['push', 'feedback', 'crashes', 'attribution', 'users', 'events', 'remote-config', 'sessions', 'location', 'views', 'apm', 'content']) {
           expect(consentInRequest[key], true);
         }
-        expect(consentInRequest.length, Platform.isAndroid ? 14 : 11);
+        expect(consentInRequest.length, Platform.isAndroid ? 15 : 12);
       } else if ((Platform.isAndroid && (i == 3 || i == 8)) || (Platform.isIOS && i == 1)) {
         expect(queryParams['events']?[0].contains('[CLY]_orientation'), true);
       } else if ((Platform.isAndroid && i == 1) || (Platform.isIOS && i == 2)) {
