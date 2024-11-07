d121299b93b8aad049937b08baff96b5d96f9ebe
ijunaid <mjunaid.akram@yahoo.com>
2024-11-07 19:27:20 +0500

    fixes and changes after running script

--- example/integration_test/event_tests/event_utils.dart
+++ example/integration_test/event_tests/event_utils.dart
@@ -1,4 +1,4 @@
-import 'package:countly_flutter/countly_flutter.dart';
+import 'package:countly_flutter_np/countly_flutter.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'dart:io';
 
