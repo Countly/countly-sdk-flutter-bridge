585861961fffe58d3e9a6fd074dc83e738c563b9
Marvin März <marvin.maerz@quickbird.io>
2025-02-14 16:37:24 +0100

    fix: remove registerWith function because its not suported from Flutter 3.29.0

--- android/src/main/java/ly/count/dart/countly_flutter/CountlyFlutterPlugin.java
+++ android/src/main/java/ly/count/dart/countly_flutter/CountlyFlutterPlugin.java
@@ -9,7 +9,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 import ly.count.android.sdk.Countly;
 import ly.count.android.sdk.CountlyConfig;
@@ -139,15 +138,6 @@ public class CountlyFlutterPlugin implements MethodCallHandler, FlutterPlugin, A
     List<CountlyFeedbackWidget> retrievedWidgetList = null;
 
     //----------PLUGIN REGISTRATION (FlutterPlugin)-------------------
-    
-    // Required for pre Flutter 1.12 projects
-    public static void registerWith(Registrar registrar) {
-        final CountlyFlutterPlugin instance = new CountlyFlutterPlugin();
-        instance.activity = registrar.activity();
-        final Context __context = registrar.context();
-        instance.onAttachedToEngineInternal(__context, registrar.messenger());
-        log("registerWith", LogLevel.INFO);
-    }
 
     // Called from Android embedding v2
     @Override
